4212bc754f091a50669564c1c93eeb37a14176cc
unknown <kruizkk@gmail.com>
2023-11-28 14:06:18 +0100

    done with final changes

--- app.js
+++ app.js
@@ -12,8 +12,15 @@ const navigationProfileName = document.querySelector(
 );
 const process = document.querySelector(".process");
 const closeMainContent = document.querySelector(".main__content-toggle");
+const singleProcess = document.querySelectorAll(".process__single");
+const singleProcessCheckbox = document.querySelectorAll(
+  ".process__single-icon"
+);
+const counterSpan = document.getElementById("counter_value");
+const progress = document.querySelector(".progress__bar--progress");
 
 const body = document.querySelector("body");
+let count = 0;
 
 alertCloseBtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -65,4 +72,66 @@ body.addEventListener("click", (e) => {
       navigationProfile.classList.remove("active");
     }
   }
+});
+
+singleProcess.forEach((process) => {
+  process.addEventListener("click", () => {
+    singleProcess.forEach((process) => {
+      process.classList.remove("active");
+    });
+    process.classList.add("active");
+  });
+});
+
+singleProcess.forEach((process, index, array) => {
+  process.addEventListener("click", (e) => {
+    const checkbox = e.target.closest(".check_icon");
+
+    if (checkbox) {
+      if (checkbox.checked) {
+        console.log(checkbox.checked);
+
+        // Check if there is a previous element
+        const previousProcess = index > 0 ? array[index - 1] : null;
+
+        if (previousProcess) {
+          const previousCheckbox =
+            previousProcess.querySelector(".check_icon input");
+
+          // Add or remove the "active" class based on the previous checkbox state
+          if (previousCheckbox && !previousCheckbox.checked) {
+            previousProcess.classList.add("active");
+          } else {
+            previousProcess.classList.remove("active");
+          }
+        }
+
+        // Move to the next element
+        const nextProcess = process.nextElementSibling;
+
+        if (nextProcess) {
+          const nextCheckbox = nextProcess.querySelector(".check_icon input");
+
+          // Add or remove the "active" class based on the next checkbox state
+          if (!nextCheckbox || (nextCheckbox && !nextCheckbox.checked)) {
+            singleProcess.forEach((process) => {
+              process.classList.remove("active");
+              nextProcess.classList.add("active");
+            });
+          } else {
+            nextProcess.classList.remove("active");
+          }
+        }
+        count++;
+        counterSpan.innerText = count;
+        const progressWidth = (count / 5) * 100 + "%";
+        progress.style.width = progressWidth;
+      } else {
+        count--;
+        counterSpan.innerText = count;
+        const progressWidth = (count / 5) * 100 + "%";
+        progress.style.width = progressWidth;
+      }
+    }
+  });
 });
